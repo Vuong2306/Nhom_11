9ed7b97057813e8235305e9999672c35f02c1497
KhangSPR <129411252+KhangSPR@users.noreply.github.com>
2024-05-06 00:08:33 +0700

    commit

--- childfashion/views.py
+++ childfashion/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 #Create your views here.
 def index(request):
-   return render(request, 'page/home.html')
+   return render(request, 'pages/home.html')
 
 # Create your views here.
