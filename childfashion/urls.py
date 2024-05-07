9907b1ce9762320e9ad85ecb1ef566d16b5ded3c
KhangSPR <129411252+KhangSPR@users.noreply.github.com>
2024-05-07 12:53:21 +0700

    Update Layout

--- childfashion/urls.py
+++ childfashion/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from . import views  # call to url_shortener/views.py
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('menu_top/', views.menu_top, name='menu_top'),  # Đường dẫn cho trang menu_top
+    path('register/', views.register, name='register'),
+    # path('gioi-thieu/', views.about, name='about'),
+    # path('tin-tuc/', views.news, name='news'),
+    # path('san-pham/', views.products, name='products'),
+    # path('lien-he/', views.contact, name='contact'),
+    # path('khuyen-mai/', views.promotions, name='promotions'),
 ]
--- childfashion/views.py
+++ childfashion/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 def index(request):
    return render(request, 'pages/home.html')
 
+def login(request):
+    # Xử lý logic đăng nhập ở đây
+    return render(request, 'pages/login.html')
+
+def register(request):
+    # Xử lý logic đăng ký ở đây
+    return render(request, 'pages/register.html')
 # Create your views here.
+def menu_top(request):
+    # Xử lý logic cho menu_top ở đây
+    return render(request, 'pages/menu_top.html')
