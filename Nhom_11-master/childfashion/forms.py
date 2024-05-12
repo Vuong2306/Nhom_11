d121d27d4ac836f4b8ce045320206988aa0123ee
dinhquangphu32 <134756733+dinhquangphu32@users.noreply.github.com>
2024-05-12 16:04:25 +0700

    Add files via upload

---
+++ Nhom_11-master/childfashion/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Loai, BangSP
+
+class ThemLoaiForm(forms.Form):
+    TenLoai = forms.CharField(label = 'Tên thể loại', max_length=100)
+
+    def clean_TenLoai(self):
+        TenLoai = self.cleaned_data['TenLoai']
+        try:
+            Loai.objects.get(TenLoai = TenLoai)
+        except Loai.DoesNotExist:
+            return TenLoai
+        raise forms.ValidationError("Tên loại đã tồn tại")
+    def save(self):
+        Loai.objects.create(TenLoai = self.cleaned_data['TenLoai'])
+
+class TheLoai1Form(forms.ModelForm):
+
+    class Meta:
+        model = Loai
+        fields = ["TenLoai"]
+
+class TheLoai2Form(forms.ModelForm):
+
+    class Meta:
+        model = BangSP
+        fields = ["TenSP", "DonGia", "HinhAnh", "MoTa","ML"]
